c5d8d4af2f5c70870ec9944130d5d1ee7eb4ebf5
dilansatalgl <satalogludilan@gmail.com>
2026-03-24 16:47:41 +0300

    feat: initial project setup with backend and frontend

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+// middleware to parse JSON
+app.use(express.json());
+
+// test route
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
